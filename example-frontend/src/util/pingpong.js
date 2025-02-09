411cfef334f6c7c7fb22835a45883783c0a61d44
javiercoronadonarvaez <javier.cn12@gmail.com>
2025-02-08 19:02:18 -0600

    Created Dockerfiles for backend and frontend so they can communicate with each other

--- example-frontend/src/util/pingpong.js
+++ example-frontend/src/util/pingpong.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const baseURL = process.env.REACT_APP_BACKEND_URL || '/api'
+console.log('BASE URL', baseURL)
 
 export const axiosInstance = axios.create({ baseURL })
 
